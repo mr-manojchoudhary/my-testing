0d0f6983abca3c6315b639447ddec1f9c2b47a7e
mr-manojchoudhary <manojbochaliya5662@gmail.com>
2024-11-25 11:33:36 +0530

    raja babu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
             <li><Link to="/">Home</Link></li>
                 {
                     user==null ?
-                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/login">Lgin</Link></li>
 
                     :
                     <>
